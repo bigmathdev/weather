179960456409f2a96160253885d11c2fe433bc76
MATHEUS-VM\bigma <bigmath00@gmail.com>
2024-01-06 19:06:59 -0300

    :construction: raw: Adicionando dados da API

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,15 +3,21 @@ import { ref } from 'vue';
 import { Icon } from '@iconify/vue';
 import axios from 'axios';
 
-let infoWeather = ref({})
+
+const infoWeather = ref({})
+const apiDataLoaded = ref(false)
+const imageUrl = ref()
 
 axios.get('https://api.hgbrasil.com/weather?format=json-cors&key=6b858f9e')
-.then( response => {
-  infoWeather = response.data.results
-})
-.catch(error => {
-  console.log(error)
-})
+  .then(response => {
+    infoWeather.value = response.data.results
+    imageUrl.value = new URL(`../icons/${infoWeather.value.condition_slug}.svg`, import.meta.url).href
+    apiDataLoaded.value = true
+  })
+  .catch(error => {
+    console.log(error)
+  })
+
 </script>
 
 <template>
@@ -19,40 +25,28 @@ axios.get('https://api.hgbrasil.com/weather?format=json-cors&key=6b858f9e')
     <header class="flex justify-end w-full">
       <input type="checkbox" value="light" class="toggle theme-controller" />
     </header>
-    <Icon icon="line-md:sun-rising-loop" width="270" height="270" color="#FDB441" fill="#FDB441" />
     <div class="info-clima text-center pt-1">
-      <span class="text-[4rem] font-bold">28º</span>
+      <img class="w-24 h-24" v-if="apiDataLoaded" :src="imageUrl" alt="">
+      <p v-else>Carregando..</p>
+      <span class="text-[4rem] font-bold">{{infoWeather.temp }}º</span>
+
       <div class="flex gap-2">
-        <span class="text-base font-medium">Máx: 24º</span>
-        <span class="text-base font-medium">Min: 19º</span>
+        <span v-if="apiDataLoaded" class="text-base font-medium">Máx: {{infoWeather.forecast[0].max}}º</span>
+        <span v-if="apiDataLoaded" class="text-base font-medium">Min: {{infoWeather.forecast[0].min}}º</span>
       </div>
+      <span v-if="apiDataLoaded">{{ infoWeather.city }}</span>
+
     </div>
     <div class="card w-full p-4">
       <div class="flex justify-between">
         <span>Hoje</span>
         <span>Mar,10</span>
       </div>
-      <div>
-        <div>
-          <span>24ª</span>
-          <i></i>
+      <div class="flex w-full justify-between">
+        <div class="flex flex-col items-center" v-for="forecast in infoWeather.forecast">
+          <span>{{ forecast.max }}</span>
+          <Icon icon="material-symbols:cloud" />
           <span>10:00</span>
-        </div>
-        <div>
-          <span>24ª</span>
-          <i></i>
-          <span>10:00</span>
-        </div>
-        <div>
-          <span>24ª</span>
-          <i></i>
-          <span>10:00</span>
-        </div>
-        <div>
-          <span>24ª</span>
-          <i></i>
-          <span>10:00</span>
-          <span>{{ infoWeather.city_name }}</span>
         </div>
       </div>
     </div>
