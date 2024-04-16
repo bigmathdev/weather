6a0b833eedb42a4b78f445182a0a697a1697dafd
Matheus <bigmath00@gmail.com>
2024-04-16 14:29:27 -0300

    :boom: fix: revertendo mudanças

--- src/components/Search.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-const model = defineModel()
-</script>
-
-<template>
-  <input type="text" v-model="model" placeholder="Busque pela cidade" class="input input-bordered input-sm max-w-xs" />
-</template>
-
-<style></style>
